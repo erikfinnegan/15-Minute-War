573144c2b8bb189dc971820eab0fe0155ee81b90
Erik Finnegan <erik.finnegan@gmail.com>
2024-10-14 14:55:19 -0400

    gitignore

--- odalike/settings.py
+++ odalike/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=(e95jdntvp&2g!l_mctd==c$6l#+84txnms#14zao(#9^tnog'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "137.184.134.18", ".15minutewar.com"]
 
 LOGIN_REDIRECT_URL = "/regions"
 LOGOUT_REDIRECT_URL = "/"
