3e7af74e6fd02b30fed6ff6d9e7db96981034400
Erik Finnegan <erik.finnegan@gmail.com>
2024-10-14 16:48:24 -0400

    asdf

--- maingame/wsgi.py
+++ maingame/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'odalike.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'isitthis.settings')
 
 application = get_wsgi_application()
