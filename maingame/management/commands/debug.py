7529a52cee3e363b1ae4a45211d6b6e44ae34dba
Erik Finnegan <erik.finnegan@gmail.com>
2024-11-17 08:20:00 -0500

    debug

--- maingame/management/commands/debug.py
+++ maingame/management/commands/debug.py
@@ -2,18 +2,22 @@ from django.core.management.base import BaseCommand
 
 from maingame.models import UserSettings, Resource
 from django.contrib.auth.models import User
+import os
 
 class Command(BaseCommand):
     help = "Initiates a battle"
 
     def handle(self, *args, **options):
         print("IT'S DEBUG TIME BABY")
-        user = User.objects.get(username="erik")
-        user_settings = UserSettings.objects.get(associated_user=user)
+        # user = User.objects.get(username="erik")
+        # user_settings = UserSettings.objects.get(associated_user=user)
 
-        perk_dict = {"inquisition_ratexx": 5}
+        # perk_dict = {"inquisition_ratexx": 5}
 
-        if perk_dict.get("inquisition_rate") and perk_dict.get("inquisition_rate") > 0:
-            print("INQUISITION ACTIVE")
-        else:
-            print("No inquisition here")
+        # if perk_dict.get("inquisition_rate") and perk_dict.get("inquisition_rate") > 0:
+        #     print("INQUISITION ACTIVE")
+        # else:
+        #     print("No inquisition here")
+
+        eriktest = os.environ.get('DJANGO_DEBUG', 'hmm')
+        print(eriktest)
