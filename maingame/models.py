e5679b1c699ba26f5956e51075f5d407466d8ac2
Erik Finnegan <erik.finnegan@gmail.com>
2024-11-25 19:55:10 -0500

    wtf

--- maingame/models.py
+++ maingame/models.py
@@ -206,12 +206,10 @@ class Dominion(models.Model):
 
     @property
     def building_primary_cost(self):
-        # return self.building_primary_cost_per_acre * self.acres
         return 1000
     
     @property
     def building_secondary_cost(self):
-        # return self.building_secondary_cost_per_acre * self.acres
         return 100
     
     @property 
