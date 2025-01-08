a2dff3b65094ab218cd8b7adfb48f4ab891691e5
Erik Finnegan <erik.finnegan@gmail.com>
2025-01-08 15:15:37 -0500

    Check upgrade use script

---
+++ maingame/management/commands/check_upgrade_use.py
@@ -0,0 +1,19 @@
+from random import randint
+from django.core.management.base import BaseCommand
+
+from maingame.models import UserSettings, Resource, Round, Event, Dominion, Unit, Building
+from django.contrib.auth.models import User
+import os
+
+from maingame.static_init import initialize_blessed_order_discoveries
+
+class Command(BaseCommand):
+    help = "Initiates a battle"
+
+    def handle(self, *args, **options):
+       for dominion in Dominion.objects.all().order_by("-acres"):
+            for building in Building.objects.filter(ruler=dominion):
+                print(f"{building.upgrades} - {building}")
+            
+            print()
+            print()
